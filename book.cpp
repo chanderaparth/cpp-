47c39d68d90b281a7e63a8ebe6c15836fa46e749
parth <chnderachanderaparth986@gmail.com>
2023-06-28 08:58:33 +0530

    bokk

--- book.cpp
+++ book.cpp
@@ -16,19 +16,19 @@ class book
 public:
           void input()
                {
-                           std::cout<<"\nEnter book no\tbook tytle\tprice of the book"<<"\n";
+                           std::cout<<"Enter book no\tbook tytle\tprice of the book";
                            std::cin>>bno;
                           std::cin>>btytle;
                            std::cin>>price;
             }
           void purchase()
           {
-            std::cout<<"Enter the number of copies to be purchased"<<"\n";
+            std::cout<<"Enter the number of copies to be purchased";
              int n;
             std::cin>>n;
             float total;
            total=total_cost(n);
-            std::cout<<"Total cost to be paid by the user:"<<total<<"/- rupees"<<"\n";
+            std::cout<<"Total cost to be paid by the user:"<<total<<"/- rupees";
          }
 };
 int main()
--- fin.cpp
+++ fin.cpp
@@ -26,9 +26,9 @@ public:
       { 
            std::cout<<"Enter the flight no:";
            std::cin>>fno;
-           std::cout<<"\nEnter\n*destination:";
+           std::cout<<"Enter*destination:";
            std::cin>>dest;
-           std::cout<<"\n*distance:";
+           std::cout<<"*distance:";
            std::cin>>dist;
           float total;
            total=calfuel(dist);
@@ -36,11 +36,11 @@ public:
     void showinfoo()
      { 
          float total;
-       std::cout<<"\n\n\t********INFORMATION*********";
-      std::cout<<"\n*Flight number:"<<fno;
-      std::cout<<"\n*Destination:"<<dest;
-      std::cout<<"\n*Distance:"<<dist<<"k.m";
-      std::cout<<"\n*Total fuel required:"<<total<<"litr"<<"\n";
+       std::cout<<"t********INFORMATION*********";
+      std::cout<<"*Flight number:"<<fno;
+      std::cout<<"*Destination:"<<dest;
+      std::cout<<"*Distance:"<<dist<<"k.m";
+      std::cout<<"*Total fuel required:"<<total<<"litr";
      }
 };
 int main()
