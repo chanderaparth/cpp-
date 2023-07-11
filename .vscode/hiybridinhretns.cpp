02d261a78bb21003271eef6a3382d2d9e8d60180
parth <chnderachanderaparth986@gmail.com>
2023-07-11 09:04:15 +0530

    inhretns

---
+++ .vscode/hiybridinhretns.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+
+class Student {
+  protected:
+    int rollNo;
+  public:
+    void getNo(void) {
+      cout << "Enter Roll no: ";
+      cin >> rollNo;
+    }
+    void putNo(void) {
+      cout << "Roll Number: " << rollNo << endl;
+    }
+};
+
+class Test : public virtual Student {
+  protected:
+    float part1,part2;
+  public:
+    void getMarks(void) {
+      cout << "Enter Marks for Part 1: ";
+      cin >> part1;
+      cout << "Enter Marks for Part 2: ";
+      cin >> part2;
+    }
+    void putMarks(void) {
+      cout << "Marks obtained:" << endl;
+      cout << "Part1 = " << part1 << endl;
+      cout << "Part2 = " << part2 << endl;
+    }
+};
+
+class Sports : virtual public Student {
+  protected:
+    float score;
+  public:
+    void getScore(void) {
+      cout << "Enter Sports weightage: ";
+      cin >> score;
+    }
+    void putScore(void) {
+      cout << "Sports weightage: " << score << endl << endl;
+    }
+};
+
+class Result : public Test,public Sports {
+  float total;
+  public:
+    void display(void);
+};
+
+void Result::display(void) {
+  total = part1 + part2 +score;
+  putNo();
+  putMarks();
+  putScore();
+  cout << "Total score: " << total << endl;
+}
+
+int main() {
+  Result S1;
+
+  S1.getNo();
+  S1.getMarks();
+  S1.getScore();
+
+  system("cls");
+  S1.display();
+
+return 0;
+}
